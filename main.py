4fa6235b4ef4a8748e61fd52a492089f0e3a8863
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-26 16:02:40 -0400

    request and response body

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from typing import Optional, Dict
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 @app.get("/")
-def home():
+def home() -> Dict:
     return {"message": "Hello world"}
 
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    
+    return Person
