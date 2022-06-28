cea323e9a54e9dccd1926be0734ae65be60a22f4
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-27 23:23:17 -0400

    validations query

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional, Dict
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -14,11 +14,20 @@ class Person(BaseModel):
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
 
+
 @app.get("/")
 def home() -> Dict:
     return {"message": "Hello world"}
 
+
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
-    
     return Person
+
+
+@app.get("/person/detail")
+def show_person(name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)):
+
+    return {name: age}
+
